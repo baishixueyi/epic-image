687ec2fac58272d81020206518de8239bbc5b852
life_liu <liulili@ruyigou.com>
2020-10-10 15:31:20 +0800

    设置路径为相对路径

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import {
-  BrowserRouter as Router
+  HashRouter as Router
 } from 'react-router-dom'
 
 ReactDOM.render(
